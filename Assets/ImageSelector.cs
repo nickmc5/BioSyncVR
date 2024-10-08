d7e27616464f8a6e2cb81a39288772c6b0886d17
Samuel <samy.guimez@gmail.com>
2024-10-08 19:33:32 -0400

    Added Scene Changer to Main Menu
    Main Menu can now select between Beach and Mountain

--- Assets/ImageSelector.cs
+++ Assets/ImageSelector.cs
@@ -8,22 +8,28 @@ public class ImageSelector : MonoBehaviour
     public UnityEngine.UI.Image image2;  // Assign in the Inspector
     public Outline outline1;  // Assign the Outline of image1 in the Inspector
     public Outline outline2;  // Assign the Outline of image2 in the Inspector
+    public SceneChange sceneChanger;  // Reference to the SceneChange script
 
+    //private bool isImage1Selected = true;
     void Start()
     {
         // Set Image 1 as selected by default
         SelectImage1();  // Call the method to enable the outline for Image 1
     }
 
-    public void SelectImage1()  // Ensure this method is public
+    public void SelectImage1()
     {
-        outline1.enabled = true;  // Enable outline for Image 1
-        outline2.enabled = false;  // Disable outline for Image 2
+        outline1.enabled = true;
+        outline2.enabled = false;
+        sceneChanger.IsImage1Selected = true;  // Image 1 is selected
     }
 
-    public void SelectImage2()  // Ensure this method is public
+    public void SelectImage2()
     {
-        outline1.enabled = false;  // Disable outline for Image 1
-        outline2.enabled = true;   // Enable outline for Image 2
+        outline1.enabled = false;
+        outline2.enabled = true;
+        sceneChanger.IsImage1Selected = false;  // Image 2 is selected
     }
+
+    
 }
--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,11 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public bool IsImage1Selected = true;
+
+    public void Load()
+    {
+        if (IsImage1Selected)
+        {
+            LoadScene("BeachScene");
+        }
+        else
+        {
+            LoadScene("MountainScene");
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
         Debug.Log("Loading scene: " + sceneName);
-        if (sceneName == "BeachScene")
+        if (sceneName == "BeachScene" || sceneName == "MountainScene")
         {
             Debug.Log(SessionSettings.sessionLength);
         }

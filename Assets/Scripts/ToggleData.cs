63e7240d5e70cfd74d6c296df5c6eb5a54e71ac1
Samuel <samy.guimez@gmail.com>
2024-11-01 22:48:44 -0400

    Bug fix
    fixed session history button flicker

--- Assets/Scripts/ToggleData.cs
+++ Assets/Scripts/ToggleData.cs
@@ -12,6 +12,6 @@ public class ToggleData : MonoBehaviour
     }
     public void ToggleDataMenu()
     {
-        DataPanel.SetActive(!DataPanel.activeSelf);
+        DataPanel.gameObject.SetActive(true);
     }
 }
